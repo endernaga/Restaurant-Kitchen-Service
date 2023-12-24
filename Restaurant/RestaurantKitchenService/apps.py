4ea0d625acaa99263db70a42460f68aa028f09bb
Misha Shushman <endernagashushman@gmail.com>
2023-12-24 14:43:59 +0200

    created project and app, also created models.py

---
+++ Restaurant/RestaurantKitchenService/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RestaurantkitchenserviceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'RestaurantKitchenService'
---
+++ Restaurant/RestaurantKitchenService/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Cook(AbstractUser):
+    year_of_expirience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cooker"
+        verbose_name_plural = "cookers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=65)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook)
